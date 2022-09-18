409d9be3e6edc3fb0ea61085365f363e3c87ddc4
eemreceliik <107258741+eemreceliik@users.noreply.github.com>
2022-09-18 23:38:00 +0300

    shoot

--- BasketBattle/Assets/Scripts/BallControll.cs
+++ BasketBattle/Assets/Scripts/BallControll.cs
@@ -2,22 +2,75 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BallControll : MonoBehaviour
 {
 
-    public float ballSpeed;
+    //public float ballSpeed;
     private Rigidbody _rb;
+    public Transform rightHand,leftHand,hero;
+    public Transform ballRightHand, ballLefthHand, BallHero;
+    public float speed = 1f;
+    public GameObject player;
+    private Rigidbody heroRb;
+
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        
-        
+        heroRb = player.GetComponent<Rigidbody>();
+        //transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
     }
 
 
     private void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetPlayerPosition();
+
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            SetPlayerPosition();
+            speed += speed * Time.deltaTime;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            var mousePosition = Input.mousePosition;
+            mousePosition.z = 10;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            SetForce(mousePosition);
+            PlayerSetForce(mousePosition);
+
+        }
+
+     
 
     }
+
+    private void SetForce(Vector3 mousePosition)
+    {
+        _rb.velocity += mousePosition * speed;
+    }
+    private void PlayerSetForce(Vector3 mousePosition)
+    {
+        heroRb.velocity += (-mousePosition * speed)/2;
+        speed = 1f;
+
+    }
+
+    private void SetPlayerPosition()
+    {
+        hero.position = BallHero.position - new Vector3(0, 0.6f, 0);
+        rightHand.position = ballRightHand.position;
+        leftHand.position = ballLefthHand.position;
+        
+    }
+
+
 }
--- BasketBattle/Assets/Scripts/PlayerMovement.cs
+++ BasketBattle/Assets/Scripts/PlayerMovement.cs
@@ -1,36 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{   
-    
-    public Transform checkPoint;
-    public GameObject ball;
-    private float ballSpeed = 1f;
+{
+    private Rigidbody heroRb;
 
     private void Start()
     {
-        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        heroRb = GetComponent<Rigidbody>();
     }
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            var screenPoint = (Input.mousePosition);
-            screenPoint.z = 10.0f; //distance of the plane from the camera
-            transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
-        }
 
-        if (Input.GetMouseButton(0))
-        {
-            ballSpeed += ballSpeed + Time.deltaTime;
-        }
 
-        if (Input.GetMouseButtonUp(0))
-        {
-            Instantiate(ball, checkPoint.position, Quaternion.identity);
-        }
-        
-    }
 }
